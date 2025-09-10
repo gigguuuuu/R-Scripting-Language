f2b134c03dde0402d01a1204dc7d46cc2458fe11
gigguuuuu <fathimanaseera321@gmail.com>
2025-09-10 07:32:11 -0500

    Add files via upload
    updated version printing is not working thouhh some kind of pointer leak or memeory issue will come stronger !

--- scipter.c
+++ scipter.c
@@ -1,34 +1,76 @@
+#include <complex.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+typedef struct{
+    // var storage & var name storage for "for loop !";
+    char *var_name;
+    int var_value;
+    char *var_char_value;
+    // for char var storage;
+}variables;
 
 char** lexer(char *code){
     int count = 0;
     int cnt_args = 0;
     int store_size = 3;
-    char *token = strtok(code," ;\n");
+    int i = 0;
     char **args = malloc(store_size * sizeof(char *));
-    while (token!=NULL){
-        if (count >= store_size){
-            store_size *= 2;
-            char **temp_mem = realloc(args,store_size * sizeof(char *));
-            if (!temp_mem){
-                printf("Memory Allocation Failed !");
-                free(args);
-                exit(1);
+    if (!args){
+        printf("Memory Allocation failed !");
+        exit(1);
+    }
+    while(code[i]!='\0'){
+        if (code[i]==' ' || code[i] == ';'|| code[i]=='\n'){
+            i++;
+            continue;
+        }
+        else if (code[i]=='"'){
+            i++;
+            int j = 0;
+            char buffer[256];
+            while(code[i]!='"' && code[i]!='\0'){
+                buffer[j++] = code[i++];
             }
-            args = temp_mem; 
+            buffer[j] = '\0';
+            if(code[i]==' '){
+                i++;
+            }
+            if (cnt_args >= store_size) {
+                store_size *= 2;
+                char **temp = realloc(args, store_size * sizeof(char *));
+                if (!temp){
+                    printf("memory allocation failed !");
+                    for (int i = 0; i < cnt_args; i++) {
+                        free(args[i]);
+                    }
+                    free(args);
+                    exit(1);
+                }
+                args = temp;
+            }
+            args[cnt_args++] = strdup(buffer); // store token
+            continue;
         }
-        args[cnt_args] = strdup(token);
-        if (!args[cnt_args]){
-            printf("Token Copying Has Failed !");
-            for (int i = 0; i < count; i++) free(args[i]);
-            free(args);
-            return NULL;
+        else if (code[i]!=' ' && code[i] != ';' && code[i] != '\n'){
+            int j = 0;
+            char buffer[256];
+            while(code[i] != ' ' && code[i] != ';' && code[i] != '\n'){
+                buffer[j++] = code[i++];
+            }
+            if  (cnt_args >= store_size) {
+                store_size *= 2;
+                args = realloc(args, store_size * sizeof(char *));
+                if (!args){
+                    printf("Memory Allocation Failed !");
+                    exit(1);
+                }
+            }
+            buffer[j] = '\0';
+            args[cnt_args++] = strdup(buffer); // store token
+            continue;
         }
-        token = strtok(NULL," ;\n");
-        cnt_args++;
-        count++;
     }
     args[cnt_args] = NULL;
     return args;
@@ -62,15 +104,54 @@ char* open_r(char *filename){
 }
 int main(int argc,char *argv[]){
     if (argc==2){
+
         char *filename = argv[1];
         char *read = open_r(filename);
+        variables vars[100];
+        int var_count = 0;
+
         if (read){
             char **lex = lexer(read);
-            printf("%s",lex[1]);
-            if (strcmp(lex[0],"let")==0){
-                printf("it's fucking work ! ");
+            int i = 0;
+            while (lex[i] != NULL && strcmp(lex[i], "stop:") != 0){
+                if (strcmp(lex[i],"let")==0){
+                    if (lex[i + 1] && lex[i + 3] && strcmp(lex[i + 2],"=")==0){
+                        if (lex[i + 3][0] == '"'){
+                            char *str_val = lex[i + 3];
+                            size_t len = strlen(str_val);
+
+                            if (len > 1 && str_val[len - 1] == '"') {
+                                str_val[len - 1] = '\0';
+                            }
+                            vars[var_count].var_name = strdup(lex[i + 1]);
+                            vars[var_count].var_char_value = lex[i + 3];
+                            var_count++;
+                        }
+                        else{
+                            vars[var_count].var_name = strdup(lex[i + 1]);
+                            vars[var_count].var_value = atoi(lex[i + 3]);
+                            var_count++;
+                        }
+
+                    }
+                }
+                else if (strcmp(lex[i],"print")==0){
+                    printf("print is working on a point !\n");
+                    for (int k = 0;k < var_count;k++){
+                        if (strcmp(lex[i + 1],vars[k].var_name)==0){
+                            if (vars[k].var_char_value){
+                                printf("%s\n",vars[k].var_char_value);
+                            }
+                            else{
+                                printf("%d",vars[k].var_value);
+                                }
+                            }
+                        }
+                    }
+                i++;
+            } 
             }
-        }
+                     
         else if (strcmp(read,"error")==0){
             printf("RCC Got An Error: Can Not Read File OR  File Does Not Exsits !");
         }
